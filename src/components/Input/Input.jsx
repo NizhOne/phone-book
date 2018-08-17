fdab894b53f660abecddf9abf4f84d2611a69a8c
nizhone <maxozol@yandex.ua>
2018-08-17 12:45:13 +0300

    css file imported

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './input.css';
+
 const Input = ({type, name, onChange, placeholder, value}) =>
     <label className={`label-input`}>
         <input type={type}
